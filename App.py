f3006ff31d915a36d7f6415f365672fff69a3374
MadLad <41120880+MegaDoot@users.noreply.github.com>
2019-01-25 09:59:24 +0000

    Track and Track Frame objects added

--- App.py
+++ App.py
@@ -1,18 +1,38 @@
 import tkinter as tk
 
+style = lambda size = 10: {
+    "bg":"#ffffff",
+    "fg":"#111111",
+    "font":("Sans Serif", size)
+}
+
 class App(tk.Tk):
   def __init__(self):
     super().__init__()
+    
     self.canvas = tk.Canvas(self, bd = 0, bg = "black")
     self.song_frame = tk.Frame(self.canvas, bg = "black")
     self.song_scrollbar = tk.Scrollbar(self, orient = "vertical", command = self.canvas.yview)
     self.canvas.configure(yscrollcommand = self.song_scrollbar.set)
+    
     self.mainloop()
 
 class Track:
-  def __init__(self, name, length, stagger, volume_mod):
+  def __init__(self, name, track_length, trim_values, volume_modifier):
     self.name = name
-    
+    self.length = track_length
+    self.trim = trim_values[:]
+    self.volume_mod = volume_modifier
+  
+  def play(self):
+    pass
+
+class TrackFrame(tk.Frame):
+  def __init__(self, parent, track):
+    super().__init__(parent)
+    text = (track.name, "-{}, -{}".format(*track.trim), track.length, track.volume_mod)
+    for i in range(2)
+      tk.Label(**style(20), text = track[i]).grid(row = 0, column = i)
 
 if __name__ == "__main__":
   app = App()
