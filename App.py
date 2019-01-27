37826e52d232bb5a273404e9ddfb680711abb202
Alex <16scorzaa@cedarsupper.co.uk>
2019-01-27 22:18:15 +0000

    Working ordering

--- App.py
+++ App.py
@@ -3,6 +3,8 @@ Usage:
   Shift to change mode
   Up and down buttons to change selected track
   Left and right to change selected attribute (edit mode only)
+
+https://pythonhosted.org/pyglet/programming_guide/controlling_playback.html
 """
 
 import tkinter as tk
@@ -17,11 +19,13 @@ paths = os.environ["PATH"].split(";")
 if not py_path in paths:
   os.environ["PATH"] += py_path + ";"
   print("Added python to PATH")
-try:
-  import pyglet
-except ImportError:
-  os.system("echo pyglet not found; installing & python -m pip install pyglet")
-  import pyglet
+
+for lib in ("pyglet", "mutagen"):
+  try:
+    exec("import " + lib)
+  except ImportError:
+    os.system("echo {0} not found; installing & python -m pip install {0}".format(lib))
+    exec("import " + lib)
 
 tracks_path = os.path.dirname(__file__) + r"\Tracks"
 attr_names = ("State", "Loop", "Name", "Trim", "Duration","Volume", "Fade Time")
@@ -31,8 +35,8 @@ bg = "#212121"
 fg = "#afafaf"
 chars = "4;ar" #Pause, play, tick, cross in Webdings font
 key_move = {
-  "<Right>":(lambda column:(0, 2 if column in (3 , 6) else 1)),
-  "<Left>":(lambda column:(0, -2 if column in (1 , 5) else -1)),
+  "<Right>":(lambda column:(0, 2 if column in (6, 1, 3) else 1)),
+  "<Left>":(lambda column:(0, -2 if column in (1, 3 , 5) else -1)),
   "<Up>":(lambda column:(-1, 0)),
   "<Down>":(lambda column:(1, 0))
 }
@@ -81,7 +85,8 @@ class App(tk.Tk):
     ttk_style.theme_use("default")
     ttk_style.configure("TProgressbar", thickness = 5)
 
-    self.tracks = [Track("Track #{}".format(i + 1), random.randint(5, 10)) for i in range(20)]
+    os.chdir("Tracks")
+    self.tracks = [Track(os.listdir()[i], random.randint(5, 10), i) for i in range(len(os.listdir()))]
     self.selection = (0, 0) #Selected track, selected attribute (edit mode only)
     self.track_selection = (0, 0)
     self.modulo = (len(self.tracks), len(column_widths))
@@ -132,6 +137,7 @@ class App(tk.Tk):
     for i in range(len(self.tracks)):
       self.track_frames.append(TrackFrame(self.song_frame, self.tracks[i]))
       self.track_frames[i].grid(row = i, column = 0)
+    print(self.track_frames[0].track)
     self.track_frames[0].highlight = range(len(column_widths))
 
     self.play_thread = PlayThread(1, self.tracks[0], self)
@@ -140,6 +146,14 @@ class App(tk.Tk):
     self.inc_mode(increment = 0)
     
     self.mainloop()
+
+  def save(self):
+    order = [track.num for track in self.tracks]
+    print(order)
+    os.chdir(os.path.dirname(__file__))
+    with open(r"Config\Order.txt", "w") as file:
+      for n in order:
+        file.write(str(n) + "\n")
   
   def update_bar(self, *events):
     self.time_lbl.config(text = "{} / {}".format(to_minutes(self.progress_dv.get()), to_minutes(len(self.tracks[self.track_selection[0]]))))
@@ -162,20 +176,32 @@ class App(tk.Tk):
       self.play_thread.play()
     
   def inc_mode(self, event = None, increment = 1):
-    self.mode = (self.mode + increment) % 3
-    self.mode_lbl.config(text = modes[self.mode])
-    self.change_selection(lambda column:(0, 0))
+    if not self.tracks[self.selection[0]].playing:
+      self.mode = (self.mode + increment) % 3
+      self.mode_lbl.config(text = modes[self.mode])
+      self.change_selection(lambda column:(0, 0))
 
   def change_selection(self, change):
     change = change(self.selection[1])
+##    print("Before:", self.tracks)
     self.selection =  [(self.selection[i] + change[i]) % self.modulo[i] for i in range(2)]
 
+    if self.mode == 1: #If order
+      pos = self.selection[0]
+      edits = (pos, (pos - change[0]) % len(self.tracks))
+      self.tracks[edits[0]], self.tracks[edits[1]] = self.tracks[edits[1]], self.tracks[edits[0]]
+      print("Switching:", edits)
+      for i in range(2):
+        self.track_frames[edits[i]].destroy()
+        new = edits[i]
+        self.track_frames[edits[i]] = TrackFrame(self.song_frame, self.tracks[new])
+        self.track_frames[edits[i]].grid(row = edits[i], column = 0)
     for obj in self.track_frames:
       obj.highlight = []
     track_frame_obj = self.track_frames[self.selection[0]]
-    if self.mode == 2:
+    if self.mode == 2: #Edit mode
       track_frame_obj.highlight = [self.selection[1]]
-    else:
+    else: #Select or order mode
       track_frame_obj.highlight = range(len(column_widths))
       self.selection[1] = 1
 
@@ -187,6 +213,7 @@ class App(tk.Tk):
       ##self.update_bar()
 
   def end(self):
+    self.save()
     if self.tracks[self.selection[0]].playing:
       print("Running")
       self.end = True
@@ -224,7 +251,7 @@ class PlayThread(threading.Thread):
     track_frame_obj.playing_state_set(False)
 
 class Track:
-  def __init__(self, name, track_length, trim_values = (0, 0), volume_modifier = 100, fade_time = 0, loop = False):
+  def __init__(self, name, track_length, num, trim_values = (0, 0), volume_modifier = 100, fade_time = 0, loop = False):
     self.name = name
     self.length = track_length
     self.trim = trim_values[:]
@@ -232,12 +259,14 @@ class Track:
     self.fade = fade_time
     self.loop = loop
     self.playing = False
+    self.num = num
 
   def __len__(self):
     return self.length
 
   def __repr__(self):
-    return "(Name: '{}', Length: {}, Trim: {}, Volume Modifier: {}, Fade Time: {}, Loop: {}, Playing: {})".format(self.name, to_minutes(self.length), self.trim, self.volume, self.fade, self.loop, self.playing)
+    return self.name
+##    return "(Name: '{}', Length: {}, Trim: {}, Volume Modifier: {}, Fade Time: {}, Loop: {}, Playing: {})".format(self.name, to_minutes(self.length), self.trim, self.volume, self.fade, self.loop, self.playing)
 
 class TrackFrame(tk.Frame):
   def __init__(self, parent, track):
@@ -254,6 +283,9 @@ class TrackFrame(tk.Frame):
         style_dict["font"][0] = "Webdings"
       tk.Label(self, **style_dict, text = self.text[i]).grid(row = 0, column = i, sticky = "NESW")
     self._highlight = []
+
+  def __repr__(self):
+    return "F " + repr(self.track)
 
   def update_text(self):
     self.text = (chars[0], chars[self.track.loop + 2], "'{}'".format(add_ellipses(self.track.name)), "(-{}s, -{}s)".format(*self.track.trim), to_minutes(len(self.track)), "{}%".format(self.track.volume), "{}s".format(self.track.fade))
@@ -277,3 +309,4 @@ class TrackFrame(tk.Frame):
 
 if __name__ == "__main__":
   app = App()
+  self = app
